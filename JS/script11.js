54b64ba0e4b8fb1ef8bc02409f2fe6219c2254fc
SosnaO <73584691+SosnaO@users.noreply.github.com>
2021-09-14 21:32:44 +0300

    11

--- JS/script11.js
+++ JS/script11.js
@@ -1,17 +1,23 @@
+document.querySelector('#btnInsert').addEventListener('click', addRow);
+document.querySelector('#btnRemove').addEventListener('click', removeRow);
 
+function addRow() {
+    const tbody = document.getElementById("sampleTable");
+    const row = document.createElement("tr")
+    const td1 = document.createElement("td")
 
-// 11. Напишите методы добавления/удаления строчки внизу таблицы с текстом 'Row{N} cell{K}' (N/K динамические числа пример Row{3} cell{1}, Row{3} cell{2}..., то есть если количество колонок в одной строчке будет изменено ваш код должен работать)
+    const rowsCount = tbody.rows.length;
+    let newRowNumber = rowsCount + 1;
 
-{/* <head> 
-<meta charset=utf-8 /> 
-<title>Insert row in a table - w3resource</title> 
-</head><body> 
-<table id="sampleTable" border="1"> 
-<tr><td>Row1 cell1</td> 
-<td>Row1 cell2</td></tr> 
-<tr><td>Row2 cell1</td> 
-<td>Row2 cell2</td></tr> 
-</table><br> 
-<button id="btnInsert" type="button" value="Insert row">  
-<button id="btnRemove" type="button" value="Remove row">  
-</body> */}
+    td1.appendChild(document.createTextNode(`Row${newRowNumber} cell1`))
+    const td2 = document.createElement("td")
+    td2.appendChild (document.createTextNode(`Row${newRowNumber} cell2`))
+    row.appendChild(td1);
+    row.appendChild(td2);
+    tbody.appendChild(row);
+}
+function removeRow(){
+    const table = document.getElementById("sampleTable");
+    const rowCount = table.rows.length;
+    table.deleteRow(rowCount -1);
+}
